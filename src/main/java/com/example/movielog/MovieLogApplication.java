e0c1dc5ea4c737c08cc94719f75186ff9550bebc
sunny <inseon0731@naver.com>
2022-05-04 20:37:38 +0900

    initial commit

---
+++ src/main/java/com/example/movielog/MovieLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.movielog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movielog/MovieLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movielog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
