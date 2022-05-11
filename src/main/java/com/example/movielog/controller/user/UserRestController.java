dc07407b0a607ac49ffe5d1983c6c699d706cfef
sunny <inseon0731@naver.com>
2022-05-11 18:02:55 +0900

    refactor: 회원가입, 로그인 동작 service 단으로 이동

--- src/main/java/com/example/movielog/controller/user/UserRestController.java
+++ src/main/java/com/example/movielog/controller/user/UserRestController.java
@@ -1,45 +1,27 @@
 package com.example.movielog.controller.user;
 
 import com.example.movielog.model.user.User;
-import com.example.movielog.repository.user.UserRepository;
-import com.example.movielog.security.JwtTokenProvider;
 import com.example.movielog.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
 public class UserRestController {
 
-  private final PasswordEncoder passwordEncoder;
-  private final JwtTokenProvider jwtTokenProvider;
-  private final UserRepository userRepository;
   private final UserService userService;
 
   @PostMapping("/join")
-  public Long join(@RequestBody Map<String, String> user){
-    return userRepository.save(User.builder()
-            .email(user.get("email"))
-            .password(passwordEncoder.encode(user.get("password")))
-            .nickname(user.get("nickname"))
-            .roles(Collections.singletonList("ROLE_USER"))
-            .build()).getId();
+  public ResponseEntity<User> join(@RequestBody User user) {
+    User joinUser = userService.join(user.getEmail(), user.getPassword(), user.getNickname());
+    return ResponseEntity.ok().body(joinUser);
   }
+
 
   @PostMapping("/login")
-  public String login(@RequestBody Map<String, String> user){
-    User member = userRepository.findByEmail(user.get("email"))
-            .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 email 입니다."));
-
-    if(!passwordEncoder.matches(user.get("password"), member.getPassword())){
-      throw new IllegalArgumentException("잘못된 비밀번호 입니다.");
-    }
-    return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
+  public String login(@RequestBody User user){
+    return userService.login(user.getEmail(), user.getPassword());
   }
-
 }
--- src/main/java/com/example/movielog/service/UserService.java
+++ src/main/java/com/example/movielog/service/UserService.java
@@ -2,12 +2,12 @@ package com.example.movielog.service;
 
 import com.example.movielog.model.user.User;
 import com.example.movielog.repository.user.UserRepository;
+import com.example.movielog.security.JwtTokenProvider;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,9 +16,34 @@ public class UserService {
 
   private final PasswordEncoder passwordEncoder;
 
-  public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
+  private final JwtTokenProvider jwtTokenProvider;
+
+  public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider){
     this.userRepository = userRepository;
     this.passwordEncoder = passwordEncoder;
+    this.jwtTokenProvider = jwtTokenProvider;
+  }
+
+  @Transactional
+  public User join(String email, String password, String nickname){
+    return userRepository.save(User.builder()
+            .email(email)
+            .password(passwordEncoder.encode(password))
+            .nickname(nickname)
+            .roles(Collections.singletonList("ROLE_USER"))
+            .build());
+  }
+
+  @Transactional
+  public String login(String email, String password){
+    User user = userRepository.findByEmail(email)
+            .orElseThrow(()-> new IllegalArgumentException("가입되지 않은 email입니다."));
+
+    if(!passwordEncoder.matches(password, user.getPassword())){
+      throw new IllegalArgumentException("잘못된 비밀번호 입니다.");
+    }
+
+    return jwtTokenProvider.createToken(user.getUsername(), user.getRoles());
   }
 
 }
