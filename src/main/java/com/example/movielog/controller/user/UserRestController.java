216ecb5a1b39c2377e441fb9b4d961e9a940b9de
sunny <inseon0731@naver.com>
2022-05-19 10:43:12 +0900

    refactor: USER의 /user/me page 추가

--- src/main/java/com/example/movielog/controller/user/UserRestController.java
+++ src/main/java/com/example/movielog/controller/user/UserRestController.java
@@ -10,8 +10,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 
 @RequiredArgsConstructor
 @RestController
@@ -45,10 +43,20 @@ public class UserRestController {
     return ResponseEntity.ok().body(joinResponse);
   }
 
+  @GetMapping("/user/me")
+  public ResponseEntity<UpdatePageResponse> updateMePage(@AuthenticationPrincipal UserAccount userAccount){
+    User user = userService.findByEmail(userAccount.getUsername())
+            .orElseThrow(()-> new UsernameNotFoundException("존재하지 않는 회원입니다."));
+
+    UpdatePageResponse updatePageResponse = new UpdatePageResponse(user.getEmail(), user.getNickname());
+
+    return ResponseEntity.ok().body(updatePageResponse);
+  }
+
 
   @PostMapping("/user/me")
   public ResponseEntity<User> updateMe(@AuthenticationPrincipal UserAccount userAccount,
-                                              @RequestBody UserUpdateRequest userUpdateRequest){
+                                       @RequestBody UserUpdateRequest userUpdateRequest){
 
     User user = userService.findByEmail(userAccount.getUsername())
             .orElseThrow(()-> new UsernameNotFoundException("존재하지 않는 회원입니다."));
