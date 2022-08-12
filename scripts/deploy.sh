3ebde74fa7bfe94330dfd5017a9c387137aaba2a
sunny <inseon0731@naver.com>
2022-08-12 21:46:59 +0900

    chore: 무중단 배포 스크립트 수정 5

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ CURRENT_PID=$(pgrep -fl movie-log | grep jar | awk '{print $1}')
 echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
 
-if [ -z $CURRENT_PID ]; then
+if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
     echo "> kill -15 $CURRENT_PID"
@@ -21,9 +21,8 @@ else
 fi
 
 
-
 echo "> 새 어플리케이션 배포"
-JAR_NAME=$(ls $REPOSITORY/jar/ | grep 'movie-log' | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 echo "> $JAR_NAME 에 실행 권한 추가"
@@ -32,6 +31,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-	-Dspring.config.location=/classpath:/application.yml,home/ec2-user/app/application-real-db.yml,classpath:/application-real.yml \
+	-Dspring.config.location=classpath:/application.yml,home/ec2-user/app/application-real-db.yml,classpath:/application-real.yml \
 	-Dspring.profiles.active=real \
 	$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
